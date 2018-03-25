c680326c67828f937390dae9c8cf12ed28ccf5f4
Hitesh Gautam <gautam31.hitesh@gmail.com>
2018-03-25 16:20:46 +0530

    Importing R

--- app/src/main/java/com/pankaj/maukascholars/activity/DashboardActivity.java
+++ app/src/main/java/com/pankaj/maukascholars/activity/DashboardActivity.java
@@ -3,6 +3,8 @@ package com.pankaj.maukascholars.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pankaj.maukascholars.R;
+
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
