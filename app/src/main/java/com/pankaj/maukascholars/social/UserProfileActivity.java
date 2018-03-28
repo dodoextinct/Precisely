66f20e4ee5606fffe7c608cb29d1caeeaab75e69
Pankaj-Baranwal <pankajisadeveloper@gmail.com>
2018-03-28 15:26:50 +0530

    Layout of user profile

---
+++ app/src/main/java/com/pankaj/maukascholars/social/UserProfileActivity.java
@@ -0,0 +1,23 @@
+package com.pankaj.maukascholars.social;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.pankaj.maukascholars.R;
+
+/**
+ * Project Name: 	<Mauka>
+ * Author List: 		Pankaj Baranwal
+ * Filename: 		<>
+ * Functions: 		<>
+ * Global Variables:	<>
+ * Date of Creation:    <28/03/2018>
+ */
+public class UserProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
